13c20cda11d1ddbbecf1ef6629de6e01aa4cc6a8
蓝条 <lantiao@iMac.local>
2017-11-07 17:02:08 +0800

    resource实例

--- angularjs/server/main.go
+++ angularjs/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"log"
 	"net/http"
@@ -13,8 +14,39 @@ func lesson8(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, strings.ToUpper(req.Method+"=>"+req.FormValue("name")))
 }
 
+type Data struct {
+	Name string `json:"name"`
+	Book string `json:"book"`
+}
+
+func getOne(w http.ResponseWriter, req *http.Request) {
+	list := Data{
+		Name: "lantiao",
+		Book: "angularjs_" + req.FormValue("name"),
+	}
+	b, _ := json.Marshal(list)
+	io.WriteString(w, string(b))
+}
+
+func getArr(w http.ResponseWriter, req *http.Request) {
+	list := []Data{
+		Data{
+			Name: "lantiao",
+			Book: "angularjs_" + req.FormValue("name"),
+		},
+		Data{
+			Name: "lantiao2",
+			Book: "angularjs2_" + req.FormValue("name"),
+		},
+	}
+	b, _ := json.Marshal(list)
+	io.WriteString(w, string(b))
+}
+
 func main() {
 	http.HandleFunc("/index.php", lesson8)
+	http.HandleFunc("/getOne.php", getOne)
+	http.HandleFunc("/getArr.php", getArr)
 	dir := http.Dir("../")
 	log.Printf("dir:%s", dir)
 	http.Handle("/client/", http.StripPrefix("/client/", http.FileServer(dir)))
